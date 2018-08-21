e21271682de83814e6952fa29bbaec37af93e044
Cristina Lugo <cmlugoce@syr.edu>
2018-08-21 22:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const rootURL = "https://api.github.com"
+
+function getRepositories() {
+  const name = document.getElementById("username").value
+  const uri = rootURL + "/users/" + name + "/repos"
+  const xhr = new XMHLHttpRequest()
+   xhr.addEventListener("load", displayRepositories)
+   xhr.open("Get", uri)
+   xhr.send()
+     return false;
+}
